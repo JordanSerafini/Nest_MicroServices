b003f80742fc5eaa794014ca3c0031b4c40fc0db
JordanSerafini <jordanserafini.74@gmail.com>
2024-08-27 17:04:01 +0200

    :bug: fix del user

--- authentification_service/src/app.controller.ts
+++ authentification_service/src/app.controller.ts
@@ -12,6 +12,7 @@ export class AuthController {
     return this.handleLogin(loginDto);
   }
   
+  
 
   @Post('register')
   async register(@Body() registerDto: { email: string; password: string }) {
