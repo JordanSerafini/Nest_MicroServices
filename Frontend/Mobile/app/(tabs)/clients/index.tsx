c6ed3b1173fa3106c82ad4b0483a5441037c8cbc
JordanSerafini <jordan@solution-logique.fr>
2024-09-16 16:47:28 +0200

    fix add customer modal

--- Frontend/Mobile/app/(tabs)/clients/index.tsx
+++ Frontend/Mobile/app/(tabs)/clients/index.tsx
@@ -1,3 +1,5 @@
+// IndexScreen.tsx
+
 import React, { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { DefaultTheme, Provider as PaperProvider, Portal } from "react-native-paper";
@@ -6,7 +8,7 @@ import ButtonPerso from "../../components/UI/ButtonPerso";
 import FabPerso from "../../components/UI/Fab/client/FabGroupClient";
 import ClientScreen from "./clientList";
 import MapClientScreen from "./clientMapAll";
-import AddClientModal from "./modals/AddClientModal"; 
+import AddClientModal from "./modals/AddClientModal";
 import EditClientModal from "./modals/EditClientModal";
 
 if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental) {
@@ -14,7 +16,7 @@ if (Platform.OS === 'android' && UIManager.setLayoutAnimationEnabledExperimental
 }
 
 const IndexScreen: React.FC = () => {
-  const [content, setContent] = useState("Liste" || "Carte");
+  const [content, setContent] = useState("Liste");
   const customer = {
     Id: 1262,
     name: "2 I Process",
@@ -24,10 +26,9 @@ const IndexScreen: React.FC = () => {
     postalCode: "12345",
     city: "Ville",
     note: "Note Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus dolorum, voluptate id corrupti reiciendis nihil unde rerum numquam quibusdam recusandae, odio cumque quos dolor at. Quas ipsa sed eum voluptate?",
-  }
+  };
 
   // États pour chaque modal
-  const [showAddModal, setShowAddModal] = useState(false);
   const [showEmailModal, setShowEmailModal] = useState(false);
   const [showEditClientModal, setShowEditClientModal] = useState(false);
   const [showAddClientModal, setShowAddClientModal] = useState(false);
@@ -47,12 +48,11 @@ const IndexScreen: React.FC = () => {
       property: LayoutAnimation.Properties.opacity,
     },
   };
-  
+
   // Appliquer cette animation lors des ouvertures et fermetures
   useEffect(() => {
     LayoutAnimation.configureNext(customAnimation);
   }, [showAddClientModal, showEditClientModal]);
-  
 
   const theme = {
     ...DefaultTheme,
@@ -73,14 +73,18 @@ const IndexScreen: React.FC = () => {
             mode={content === "Liste" ? "contained" : "outlined"}
             icon={"account"}
             text={"Liste"}
-            css={`w-4.5/10 self-center ${content === "Liste" ?  "bg-blue-800" : "bg-gray-100"}`}
+            css={`w-4.5/10 self-center ${
+              content === "Liste" ? "bg-blue-800" : "bg-gray-100"
+            }`}
             onPress={() => setContent("Liste")}
           />
           <ButtonPerso
             mode={content === "Carte" ? "contained" : "outlined"}
             icon={"map-marker-account"}
             text={"Carte"}
-            css={`w-4.5/10 self-center ${content === "Carte" ?  "bg-blue-800" : "bg-gray-100"}`}
+            css={`w-4.5/10 self-center ${
+              content === "Carte" ? "bg-blue-800" : "bg-gray-100"
+            }`}
             onPress={() => setContent("Carte")}
           />
         </View>
@@ -89,37 +93,36 @@ const IndexScreen: React.FC = () => {
           {content === "Liste" && <ClientScreen />}
           {content === "Carte" && <MapClientScreen />}
         </View>
-        {showAddClientModal || showEditClientModal ? (null) : (
-        <FabPerso
-          showAddModal={() => {
-            LayoutAnimation.configureNext(customAnimation);
-            setShowAddClientModal(true);
-          }}
-          showEmailModal={() => {
-            LayoutAnimation.configureNext(customAnimation);
-            setShowEmailModal(true);
-          }}
-          showEditClientModal={() => {
-            LayoutAnimation.configureNext(customAnimation);
-            setShowEditClientModal(true);
-          }}
-          showAddClientModal={() => {
-            LayoutAnimation.configureNext(customAnimation);
-            setShowAddClientModal(true);
-          }}
-        />
+        {!showAddClientModal && !showEditClientModal && (
+          <FabPerso
+            showEmailModal={() => {
+              LayoutAnimation.configureNext(customAnimation);
+              setShowEmailModal(true);
+            }}
+            showEditClientModal={() => {
+              LayoutAnimation.configureNext(customAnimation);
+              setShowEditClientModal(true);
+            }}
+            showAddClientModal={() => {
+              LayoutAnimation.configureNext(customAnimation);
+              setShowAddClientModal(true);
+            }}
+          />
         )}
         {/*---------------------------------------- Modals -----------------------------*/}
         <Portal>
           {showAddClientModal && (
-            <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
-              <AddClientModal visible={showAddClientModal} onDismiss={() => setShowAddClientModal(false)} />
-            </View>
+            <AddClientModal
+              visible={showAddClientModal}
+              onDismiss={() => setShowAddClientModal(false)}
+            />
           )}
           {showEditClientModal && (
-            <View style={{ position: 'absolute', bottom: 0, width: '100%' }}>
-              <EditClientModal visible={showEditClientModal} onDismiss={() => setShowEditClientModal(false)} customer={customer} />
-            </View>
+            <EditClientModal
+              visible={showEditClientModal}
+              onDismiss={() => setShowEditClientModal(false)}
+              customer={customer}
+            />
           )}
         </Portal>
       </SafeAreaView>
--- Frontend/Mobile/app/components/UI/Fab/client/FabGroupClient.tsx
+++ Frontend/Mobile/app/components/UI/Fab/client/FabGroupClient.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { FAB, Portal, useTheme } from 'react-native-paper';
 
 interface FabPersoProps {
-  showAddModal: () => void;
   showEmailModal: () => void;
   showEditClientModal: () => void;
   showAddClientModal: () => void;
 }
 
 const FabPerso: React.FC<FabPersoProps> = ({
-  showAddModal,
   showEmailModal,
   showEditClientModal,
   showAddClientModal
@@ -27,7 +25,6 @@ const FabPerso: React.FC<FabPersoProps> = ({
         icon={open ? 'account' : 'plus'}
         color={"white"}
         actions={[
-          { icon: 'plus', onPress: showAddModal, color: theme.colors.primary },
           { icon: 'file-send', label: 'Formulaire satisfaction', onPress: showEmailModal, color: theme.colors.primary },
           { icon: 'account-edit', label: 'Editer client', onPress: showEditClientModal, color: theme.colors.primary },
           { icon: 'account-plus', label: 'Ajouter client', onPress: showAddClientModal, color: theme.colors.primary },
