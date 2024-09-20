b0350efa0c026892e1772f5f5f6a39f90a548031
JordanSerafini <jordan@solution-logique.fr>
2024-09-20 16:11:02 +0200

    class generator

---
+++ Backend/customer_service/src/entities/CustomerAdvantage.entity.ts
@@ -0,0 +1,19 @@
+export class CustomerAdvantage {
+  Id: string;
+  CustomerId: string;
+  AdvantageDate: Date;
+  AdvantageState: number;
+  ThresholdValue: number;
+  ExpiryDate: Date;
+  CreatedByDocumentId: string;
+  UsedInDocumentId: string;
+  AppliedToDiscountId: string;
+  FreeItemId: string;
+  LoyaltyCalculationLineId: string;
+  sysCreatedDate: Date;
+  sysCreatedUser: string;
+  sysModifiedDate: Date;
+  sysModifiedUser: string;
+  DiscountOnCumulativeTurnover: number;
+  CumulativeTurnover: number;
+}
