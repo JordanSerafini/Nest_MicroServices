238a6233575443bcada2d031e7dc0a58409daeb2
Jordan S <jordanserafini.74@gmail.com>
2024-09-28 16:32:16 +0200

    :construction: setup

--- Backend/deal_service/src/app.service.ts
+++ Backend/deal_service/src/app.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class DealService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+ 
+ 
 }
--- function bash windows/recursive_folder.sh
+++ function bash windows/recursive_folder.sh
@@ -5,15 +5,7 @@ foreach ($folder in $folders) {
     npm outdated
     Write-Host "Updating packages for $($folder.FullName)"
     npm update
-    npm uninstall inflight
-    npm uninstall @humanwhocodes/config-array
-    npm uninstall @humanwhocodes/object-schema
-    npm uninstall rimraf
-    npm uninstall glob
-    npm install rimraf@latest
-    npm install lru-cache@latest
-    npm install @eslint/config-array@latest
-    npm install @eslint/object-schema@latest
+
 
 
     Pop-Location
