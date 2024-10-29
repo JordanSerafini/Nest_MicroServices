59b8919c4db49e04d4d23e83b983a2cdaf3dbad0
JordanSerafini <jordan@solution-logique.fr>
2024-10-29 17:29:19 +0100

    working :construction:

--- Backend/api_gateway/src/main.ts
+++ Backend/api_gateway/src/main.ts
@@ -4,6 +4,11 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5050',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   // Connexion au microservice item_service
   app.connectMicroservice<MicroserviceOptions>({
