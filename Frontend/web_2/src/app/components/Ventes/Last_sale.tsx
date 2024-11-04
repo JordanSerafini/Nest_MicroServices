eebf61b66552e345d7a8a2e6f024eb5401374568
JordanSerafini <jordan@solution-logique.fr>
2024-11-04 13:01:19 +0100

    Frontend > Web_2 > refacto

--- Frontend/web_2/src/app/components/Ventes/Last_sale.tsx
+++ Frontend/web_2/src/app/components/Ventes/Last_sale.tsx
@@ -1,5 +1,9 @@
+import { useDashboardContext } from "@/app/context/DashboardContext"
 
 function Last_sale() {
+  const { dashboardSearchQuery: SearchQuery } = useDashboardContext();
+  
+
   return (
     <div>Last_sale</div>
   )
