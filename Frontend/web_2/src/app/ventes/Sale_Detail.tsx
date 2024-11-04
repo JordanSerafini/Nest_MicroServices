626307e07a7662f7697fbc92c66b72d57171844d
JordanSerafini <jordan@solution-logique.fr>
2024-11-04 17:16:44 +0100

    Frontend > Web_2 > Sale Detail

--- Frontend/web_2/src/app/ventes/Sale_Detail.tsx
+++ Frontend/web_2/src/app/ventes/Sale_Detail.tsx
@@ -15,7 +15,6 @@ const DetailView: React.FC<DetailViewProps> = ({ saleId }) => {
   useEffect(() => {
     const fetchDetail = async () => {
       const data = await getSaleById(saleId);
-      console.log(data.saleDocument.SaleDocumentLine);
       setSaleDetail(data.saleDocument);
     };
 
@@ -25,28 +24,39 @@ const DetailView: React.FC<DetailViewProps> = ({ saleId }) => {
   if (!saleDetail) return <p>Chargement...</p>;
 
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-4">Détails de la vente</h1>
-      <p><strong>ID de la vente :</strong> {saleId}</p>
-      <p><strong>Client :</strong> {saleDetail.CustomerId}</p>
-      <p><strong>Montant Total :</strong> {saleDetail.TotalDueAmount} €</p>
-    
+    <div className="">
+      {/* --------------------------------------------------------------------------- Informations de la vente  */}
+      <h1 className="text-3xl font-bold mb-4 text-center text-black">
+        Détails de la vente
+      </h1>
+      
+      <p>
+        <strong className="text-black">Client :</strong> {saleDetail.CustomerId}
+      </p>
+      
+
+      {/* --------------------------------------------------------------------------- Tableau  */}
       <div className="overflow-x-auto mt-6">
         <table className="min-w-full bg-white border border-gray-200">
           <thead>
-            <tr className="bg-white border-b border-gray-300">
-              <th className="px-4 py-2 w-6/10 text-center border-r">Description</th>
-              <th className="px-4 py-2 w-0.5/10 text-center border-r">Quantité</th>
+            <tr className="bg-white border-b border-gray-300 text-black">
+              <th className="px-4 py-2 w-6/10 text-center border-r">
+                Description
+              </th>
+              <th className="px-4 py-2 w-0.5/10 text-center border-r">
+                Quantité
+              </th>
               <th className="px-4 py-2 w-1/10 text-center">Prix TTC</th>
             </tr>
           </thead>
           <tbody>
-            {saleDetail.SaleDocumentLine && saleDetail.SaleDocumentLine.length > 0 ? (
+            {saleDetail.SaleDocumentLine &&
+            saleDetail.SaleDocumentLine.length > 0 ? (
               saleDetail.SaleDocumentLine.map((line, index) => (
                 <tr key={index} className="border-b border-gray-200">
                   <td className="px-4 py-2 text-sm text-gray-700 border-r max-h-20 overflow-y-auto">
                     <div className="max-h-20 overflow-y-auto">
-                      {line.Description || "Aucune description"}
+                      {line.Description || "description manquante"}
                     </div>
                   </td>
                   <td className="px-4 py-2 text-sm text-center text-gray-700 border-r">
@@ -66,9 +76,17 @@ const DetailView: React.FC<DetailViewProps> = ({ saleId }) => {
             )}
           </tbody>
         </table>
+        <div className="flex items-center gap-2 pt-2">
+          <div className="w-9/10"></div>
+          <p className="text-base font-bold text-black border-r">
+            Total : 
+          </p>
+          <p className="">{saleDetail.TotalDueAmount} €</p>
+        </div>
       </div>
     </div>
   );
+  
   
   
 };
